13161748fa3f69db3056677ab1e567eced731267
Wintermotes <patri-kk@hotmail.com>
2016-06-15 17:40:21 +0200

    Rebased and merge trying

--- unity/Assets/Scripts/weapons/RLauncherBehaviour.cs
+++ unity/Assets/Scripts/weapons/RLauncherBehaviour.cs
@@ -3,8 +3,23 @@ using System.Collections;
 
 public class RLauncherBehaviour : Weapon {
 
+    public Transform weaponEdge;
+    public LayerMask whatToHit;
+    public GameObject rocketProjectile;
+    public float speed = 300.0f; 
+
+    private GameObject m_rocket; 
+
     override
     public void Shoot() {
         base.Shoot();
+
+        CreateRocketTrail();
+
+    }
+
+    void CreateRocketTrail()
+    {
+        Instantiate(rocketProjectile, weaponEdge.transform.position, weaponEdge.rotation);
     }
 }

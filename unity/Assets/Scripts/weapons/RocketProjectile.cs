5fe747e58bf8a6e531238b260e300d0945038291
Wintermotes <patri-kk@hotmail.com>
2016-06-17 21:36:51 +0200

    Minor debugging, and more rocket projectile work

--- unity/Assets/Scripts/weapons/RocketProjectile.cs
+++ unity/Assets/Scripts/weapons/RocketProjectile.cs
@@ -3,8 +3,23 @@ using System.Collections;
 
 public class RocketProjectile : MonoBehaviour {
 
+    /* The rocket should
+     * Have exploding particles 
+     * Shake the screen
+     * Apply damage
+     * // Should be moved to weapon 
+     * Have a cooldown timer 
+     * Have smoke from gun 
+     * Signify slider for holding down 
+     * Trigger animation on fire
+     */
+
     public float timeBeforeDestroyed = 1;
     public float speed = 10.0f;
+    public int rocketDamage = 30;
+    public float m_radius = 5.0f;
+    public float explosionForce;
+    public LayerMask thingsToExplode; 
 
     public GameObject body, player; 
 
@@ -18,7 +33,6 @@ public class RocketProjectile : MonoBehaviour {
 
         player = GameObject.FindWithTag("Player");
         body = player.transform.GetChild(1).gameObject;
-        Debug.Log("body ? : " + body.name); 
 
         Physics2D.IgnoreCollision(GetComponent<Collider2D>(), body.GetComponent<Collider2D>());
         Physics2D.IgnoreCollision(GetComponent<Collider2D>(), player.GetComponent<Collider2D>());
@@ -31,16 +45,53 @@ public class RocketProjectile : MonoBehaviour {
         rb.AddForce(transform.right * speed);
     }
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red; 
+        Gizmos.DrawWireSphere(transform.position, m_radius);
+    }
+
     void OnCollisionEnter2D(Collision2D coll)
     {
-        Debug.Log(coll.gameObject.name);
         m_source.Play();
 
-        
+        Collider2D[] objects = Physics2D.OverlapCircleAll(transform.position, m_radius, thingsToExplode);
+
+        foreach (Collider2D m_obj in objects) {
+            Rigidbody2D temp_rb = m_obj.GetComponent<Rigidbody2D>();
+
+            if (m_obj.gameObject.tag == "Enemy")
+            {
+                Debug.Log("Applying damage"); 
+                StartCoroutine(ShatterRocket());
+                m_obj.gameObject.SendMessage("ApplyDamage", rocketDamage);
+
+            }
+
+            if (temp_rb != null) {
+
+                Vector3 deltaPos = temp_rb.transform.position - transform.position;
+                Vector3 force = deltaPos.normalized * 100.0f;
+                temp_rb.AddForce(force * explosionForce); 
+
+
+            }
+        }
+
         Destroy(gameObject, m_source.clip.length);
         Destroy(GetComponent<SpriteRenderer>());
         Destroy(GetComponent<BoxCollider2D>()); 
     }
+
+    IEnumerator ShatterRocket()
+    {
+        //m_shatterParticles.Play();
+
+        yield return new WaitForSeconds(1.0f);
+        Destroy(gameObject);
+    }
+
+
 
 
 }

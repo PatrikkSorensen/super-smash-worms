9276b2e25e751949bdf8fd6b5bbb9ecfe2c669d0
Wintermotes <patri-kk@hotmail.com>
2016-06-20 13:41:56 +0200

    Reorganizing of animations and begun tutorial scene

---
+++ unity/Assets/Scripts/ui/TutWelcome.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutWelcome : MonoBehaviour {
+
+    private Animator m_anim; 
+	// Use this for initialization
+	void Start () {
+        m_anim = GetComponent<Animator>(); 
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+            m_anim.SetTrigger("begin"); 
+
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+            m_anim.SetTrigger("exit");
+
+    }
+}

bd16a68fffa3f86f0220ce2a9830b5cfa35075fc
Wintermotes <patri-kk@hotmail.com>
2016-07-06 15:07:42 +0200

    Added new flying bomb enemy, and started working on tutorial scene and background assets

---
+++ unity/Assets/Scripts/ExplodeBomb.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplodeBomb : MonoBehaviour {
+
+    bool running = false; 
+
+    public void Explode()
+    {
+        if(!running)
+        StartCoroutine(StartExplode()); 
+    }
+
+    IEnumerator StartExplode()
+    {
+        running = true; 
+        Debug.Log("Exploding..."); 
+        yield return new WaitForSeconds(2.0f);
+        Destroy(gameObject); 
+    }
+}

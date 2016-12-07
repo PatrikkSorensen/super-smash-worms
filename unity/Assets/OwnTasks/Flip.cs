05297f077a625c448f8aa5dfaa08333ce44a231f
patrikk sørensen <patrikk srensen>
2016-12-07 21:29:58 +0100

    Finally found a descent way of flipping exactly 180 degrees

--- unity/Assets/OwnTasks/Flip.cs
+++ unity/Assets/OwnTasks/Flip.cs
@@ -1,12 +1,15 @@
 ﻿using UnityEngine;
 using BehaviorDesigner.Runtime;
 using BehaviorDesigner.Runtime.Tasks;
+using DG.Tweening; 
 
 public class FlipTowardsPlayer : Action {
 
     public override TaskStatus OnUpdate()
     {
-        transform.rotation = transform.rotation * Quaternion.Euler(0, 180, 0);
+        Vector3 vector = transform.rotation.eulerAngles;
+        Vector3 vector2 = Quaternion.AngleAxis(-45, Vector3.up) * vector;
+        transform.DORotateQuaternion(transform.rotation * Quaternion.Euler(0, 180, 0), 1.0f);
         return TaskStatus.Success; 
     }
 }
